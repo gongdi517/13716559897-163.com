e1ff2b48d82382dabec19daff9ad556c5a843d4e
宫迪 <gongdi@gongdideMacBook-Pro.local>
2016-12-08 14:34:08 +0800

    -sdds

---
+++ asdsadsad/asdsadsad/weCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  weCollectionViewCell.swift
+//  asdsadsad
+//
+//  Created by 宫迪 on 2016/12/8.
+//  Copyright © 2016年 gd. All rights reserved.
+//
+
+import UIKit
+
+class weCollectionViewCell: UICollectionViewCell {
+    
+}
